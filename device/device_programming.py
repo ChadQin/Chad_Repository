734c6bb5361c994d74a97bfc851a3e1a194d209a
Chad <qq760533763@outlook.com>
2024-02-27 16:27:45 +0800

    注释：Pyvisa控制测试设备类集合。

--- device/device_programming.py
+++ device/device_programming.py
@@ -1,5 +1,5 @@
 import time
-import pyvisa
+import pyvisa,serial
 
 #########################
 # update time: 2024-02-22
@@ -55,7 +55,6 @@ class KS34465A:
 
     def get_curr_ac(self):
         curr=self.K34465A.query('MEAS:CURR:AC?')
-        time.sleep(5)
         return(float(curr))
 
 
@@ -121,13 +120,31 @@ class IT8812B:
         # CC模式下，调节电流大小(A)
         self.IT8812B.write('CURR {}'.format(curr))
 
+    def CC_setup_curr_read(self):
+        # CC模式下，读取设定电流大小(A)
+        curr = self.IT8812B.query('CURRent?')
+        curr = round(float(curr), 3)
+        return curr
+
     def CV_setup_volt(self,volt):
         # CV模式下，调节电压大小(V)
         self.IT8812B.write('VOLT {}'.format(volt))
 
+    def CV_setup_volt_read(self):
+        # CV模式下，读取设定电压大小(V)
+        volt = self.IT8812B.query('VOLTage?')
+        volt = round(float(volt), 3)
+        return volt
+
     def CR_setup_res(self,res):
         # CR模式下，调节电阻大小(Ω)
         self.IT8812B.write('RES {}'.format(res))
+
+    def CR_setup_res_read(self):
+        # CR模式下，读取调节电阻大小(Ω)
+        res = self.IT8812B.query('RESistance?')
+        res = round(float(res), 3)
+        return res
 
     def local_control(self):
         # 锁定设备实体按键控制
@@ -141,6 +158,7 @@ class IT8812B:
             self.IT8812B.write('INP 0')
         else:
             print("输入错误，请输入'on'或者'off'")
+
     def clear_error_status(self):
         # 清除错误状态
         self.IT8812B.write('SYST:CLE')
@@ -150,4 +168,85 @@ class IT8812B:
         self.IT8812B.close()
 
 
+class Tek_osc():
+    def __init__(self,baud_rate):
+        self.rm = pyvisa.ResourceManager()
+        self.rm_address = self.rm.list_resources()
+        for i, device in enumerate(self.rm_address):
+            print(f"{i}:{device}")
+        self.tek_osc = self.rm.open_resource(self.rm_address[int(input('选择示波器串口:\n'))])
+        self.tek_osc.baud_rate = baud_rate
 
+    def as_num(self,x):
+        y = '{:.6f}'.format(x)  # .10f 保留10位小数
+        return y
+
+    def single_sequence(self,state):
+        #单次触发: SEQuence
+        #多次触发:  RUNSTop
+        self.tek_osc.write('ACQuire:STOPAfter {}\n'.format(state))
+        if state == 'SEQuence':
+            self.tek_osc.write('ACQuire:STATE ON\n')
+        else:
+            pass
+
+    def meas_read(self,type):
+        #{
+        #   频率: FREQuency
+        #   均方根: RMS
+        #
+        #
+        # }
+        self.tek_osc.write('MEASUrement:IMMed:TYPe {}\n'.format(type))
+        data = float(self.tek_osc.query('MEASUrement:IMMed:VALue?\n'))
+        data = self.as_num(data)
+        return data
+
+    def close_dev(self):
+        # 关闭设备连接
+        self.tek_osc.close()
+
+    def autoset(self):
+        self.tek_osc.write('AUTOSET EXECUTE\n')
+        # self.tek_osc.write('DISplay:INTENSITy:WAVEform 75\n')
+
+
+class CYHR_SignalSource:
+    def __init__(self,portx, bps, timeout):
+            # 打开串口，并得到串口对象
+            self.ser = serial.Serial(portx, bps, timeout=timeout)
+
+    def write_data(self, msg):
+        print('CMD: {}'.format(msg))
+        self.ser.write('{}\n'.format(msg).encode('utf-8'))
+
+    def read_data(self):
+        self.data = self.ser.readline()
+        self.data = self.data.decode('utf-8')
+        print('return info: {}\n'.format(self.data))
+        return self.data
+
+    def hex_convert(self,dec):
+        self.hex_data = hex(dec)[2:]
+        return self.hex_data.upper()
+
+    def full_8byte_hex(self,hex_data):
+        if 8 - len(hex_data) > 0:
+            self.hex_data = '0' * (8 - len(hex_data)) + hex_data
+        return self.hex_data.upper()
+
+    def confirm_connect(self):
+        self.write_data('<REA>')
+        self.read_data()
+
+    def single_output(self,freq,pow,level):
+        freq = self.full_8byte_hex(self.hex_convert(freq))
+        self.write_data('<FRQ{}>'.format(freq))
+        pow = self.hex_convert(pow)
+        self.write_data('<POW{}{}>'.format(level,pow))
+
+    def STOP(self):
+        self.write_data('<STO>')
+
+    def close_dev(self):
+        self.ser.close()
